ad71536065286bd2b061e0ecb14e0a503afc3ffc
akrxso <143086344+akrxso@users.noreply.github.com>
2023-12-27 13:32:49 +0900

    [level unrated] Title: 0 떼기, Time: 5.36 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181847. 0 떼기/0 떼기.java
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.java
@@ -1,19 +1,5 @@
 class Solution {
     public String solution(String n_str) {
-        String answer = "";
-        int count =0;
-        for(int i=0;i<n_str.length();i++)
-        {
-            if(n_str.charAt(i)!='0')
-            {
-                count=i;
-                break;
-            }
-        }
-        for(int i=count;i<n_str.length();i++)
-        {
-            answer+=n_str.charAt(i);
-        }
-        return answer;
+        return ""+Integer.parseInt(n_str);
     }
 }
