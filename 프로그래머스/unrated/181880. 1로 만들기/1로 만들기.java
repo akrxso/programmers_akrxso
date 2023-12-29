814398f1012db523d1c9e110fb6323de21af393f
akrxso <143086344+akrxso@users.noreply.github.com>
2023-12-29 21:21:41 +0900

    [level unrated] Title: 1로 만들기, Time: 0.02 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.java
+++ 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.java
@@ -3,15 +3,11 @@ class Solution {
         int answer = 0;
         for(int i=0;i<num_list.length;i++)
         {
-            while (num_list[i]!=0) {
+            while (num_list[i]!=1) {
                 if (num_list[i] % 2 == 0) {
                     num_list[i] = num_list[i] / 2;
-                    if(num_list[i]==0)
-                        break;
                 } else {
                     num_list[i] = (num_list[i] - 1)/2;
-                    if(num_list[i]==0)
-                        break;
                 }
                 answer++;
             }
